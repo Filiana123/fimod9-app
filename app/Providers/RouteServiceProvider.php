7a2042f9802a1e99880ce77f62e78d26001370f6
Filiana123 <164728654+Filiana123@users.noreply.github.com>
2024-06-23 20:28:20 +0700

    Add files via upload

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Providers;
+
+
+
+class RouteServiceProvider
+{
+    /**
+     * This is the path to the "home" route for your application.
+     * Typically, users are redirected here after login.
+     *
+     * @var string
+     */
+    public const HOME = '/';
+
+    // Other methods and properties...
+}
